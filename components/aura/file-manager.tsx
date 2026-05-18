bb20482ea39f31d4206405333df0d41819f63a04
aquilaoliveira000-max <aquilaoliveira000@gmail.com>
2026-05-17 21:15:51 -0300

    a

--- components/aura/file-manager.tsx
+++ components/aura/file-manager.tsx
@@ -500,7 +500,7 @@ export function FileManager() {
         {!selectionMode && (
           <div className="absolute bottom-2 right-2" onClick={e => e.stopPropagation()}>
             {item.type === 'file' ? (
-              <a onClick={() => handleDownloadClick(item)}
+              <button onClick={() => handleDownloadClick(item)}
                 className="w-7 h-7 rounded-full flex items-center justify-center bg-[#0d0d12] border border-[#2a2a3a] hover:border-[#3b82f6] hover:bg-[#3b82f6]/20 transition-all opacity-0 group-hover:opacity-100"
                 title="Baixar">
                 <Download size={13} className="text-[#3b82f6]"/>
