2ac370b445e32f13f4c533338fd9d0bcb2f37266
aquilaoliveira000-max <aquilaoliveira000@gmail.com>
2026-05-20 14:23:23 -0300

    a

--- components/aura/file-manager.tsx
+++ components/aura/file-manager.tsx
@@ -578,7 +578,7 @@ export function FileManager() {
               <button onClick={() => handleDownloadFolder(item)}
                 className="w-10 h-10 rounded-full flex items-center justify-center active:bg-[#1a1a24]">
                 <Download size={20} className="text-[#3b82f6]"/>
-              </a>
+              </button>
             )}
             <div onClick={e => e.stopPropagation()}>
               <ItemMenu item={item}/>
